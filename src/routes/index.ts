9e2393f44f093c441ec151baee53b22fd7205bbc
gabriella <gabriellabraga.academico@gmail.com>
2023-11-20 13:39:05 -0300

    Adiciona recurso de autenticação de usuário close #2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 const router = express.Router()
 import userRoutes from './userRoutes'
+import authRoutes from './authRoutes'
 
 router.use(userRoutes)
+router.use(authRoutes)
 
 export default router
