0236157c2abb7a18a23dd8f1fbd921f9e20b743b
marysclair <ramalho.maria@academico.ifpb.edu.br>
2023-12-13 09:36:02 -0300

    Adiciona middleware de verificação de usuário em rotas de vaga de estacionamento

--- src/routes/parkingSpaceRoutes.ts
+++ src/routes/parkingSpaceRoutes.ts
@@ -4,6 +4,7 @@ import { checkLogin } from "../middlewares/checkLogin"
 import multer  from "multer"
 import uploadConfig from "../config/upload"
 import { isOwner } from "../middlewares/isOwner"
+import { verifyUserOnParkingSpace } from "../middlewares/verifyUserOnParkingSpace"
 
 const upload = multer(uploadConfig.upload("./pictures"))
 
@@ -13,7 +14,7 @@ const parkingSpaceController = new ParkingSpaceController()
 parkingSpaceRoutes.post("/parkingSpaces", checkLogin, isOwner, upload.array("pictures"), parkingSpaceController.create)
 parkingSpaceRoutes.get("/parkingSpaces/:id", checkLogin, parkingSpaceController.detail)
 parkingSpaceRoutes.get("/parkingSpaces", parkingSpaceController.listAll)
-parkingSpaceRoutes.patch("/parkingSpaces/:id", checkLogin, isOwner, parkingSpaceController.update)
-parkingSpaceRoutes.delete("/parkingSpaces/:id", checkLogin, isOwner, parkingSpaceController.delete)
+parkingSpaceRoutes.patch("/parkingSpaces/:id", checkLogin, isOwner, verifyUserOnParkingSpace, parkingSpaceController.update)
+parkingSpaceRoutes.delete("/parkingSpaces/:id", checkLogin, isOwner, verifyUserOnParkingSpace, parkingSpaceController.delete)
 
 export default parkingSpaceRoutes
