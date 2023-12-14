6479aa0264cb361bd146ef0435d5aef2dd887944
gabriella <gabriellabraga.academico@gmail.com>
2023-12-14 00:28:07 -0300

    Modifica testes de integração do domínio de usuários

--- src/_tests_/userRoutes.test.ts
+++ src/_tests_/userRoutes.test.ts
@@ -3,7 +3,8 @@ import { prisma } from "../database/prisma"
 import app from "../app"
 
 describe("Users", () => {
-
+	let token: string
+	let userId: string
 	afterAll( async () => {
 		await prisma.user.deleteMany()
 	})
@@ -15,10 +16,11 @@ describe("Users", () => {
 			phoneNumber: "(83)99168-4630",
 			password: "Senha2023"
 		}
-
-		await request(app).post("/users").send(newUser)
-		const tokenResponse = await request(app).post("/login").send({name: newUser.name, password: newUser.password})
-		const token = tokenResponse.body.token
+	
+		const user = await request(app).post("/users").send(newUser)
+		userId = user.body.id
+		const tokenResponse = await request(app).post("/login").send({email: newUser.email, password: newUser.password})
+		token = tokenResponse.body.token
 	})
 
 	test("cadastro de usuário com todas as informações corretas", async () => {
@@ -58,15 +60,7 @@ describe("Users", () => {
 
 		const receivedUser = response.body 
 
-		expect({
-			email: newUser.email,
-			name: newUser.name,
-			password: newUser.password
-		}).toEqual({
-			email: receivedUser.email,
-			name: receivedUser.name,
-			password: receivedUser.password
-		})
+		expect(receivedUser).toBeInstanceOf(Array)
 		expect(response.status).toBe(400)
 	}, 10000)
 
@@ -74,18 +68,18 @@ describe("Users", () => {
 		const pixKey = "111.222.333-44"
 	
 		const response = await request(app)
-			.post("/upgrade")
-			.set("Authorization", `Bearer ${token}`)
+			.put(`/users/${userId}`)
+			.set("authorization", token)
 			.send({ pixKey })
 	
 		expect(response.status).toBe(200)
 	
-		expect(response.body.message).toBe("Upgrade de conta realizado com sucesso")
+		expect(response.body.pixKey).toBe(pixKey)
 	
-		await prisma.owner.findUnique({
+		await expect(prisma.owner.findUnique({
 			where: {
 				userId: response.body.userId
 			},
-		})
+		})).resolves.not.toBeNull()
 	})
 })
