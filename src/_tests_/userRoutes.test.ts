c3de6a735791aedc1c9dee5bb5772a6a91236568
gabriella <gabriellabraga.academico@gmail.com>
2023-12-13 22:01:44 -0300

    Adiciona novos testes de integração

--- src/_tests_/userRoutes.test.ts
+++ src/_tests_/userRoutes.test.ts
@@ -8,6 +8,19 @@ describe("Users", () => {
 		await prisma.user.deleteMany()
 	})
 
+	beforeEach(async () => {
+		const newUser = {
+			name: "Maria da Silva",
+			email: "mariasilva@gmail.com",
+			phoneNumber: "(83)99168-4630",
+			password: "Senha2023"
+		}
+
+		await request(app).post("/users").send(newUser)
+		const tokenResponse = await request(app).post("/login").send({name: newUser.name, password: newUser.password})
+		const token = tokenResponse.body.token
+	})
+
 	test("cadastro de usuário com todas as informações corretas", async () => {
 		const newUser = {
 			name: "Maria da Silva",
@@ -34,4 +47,45 @@ describe("Users", () => {
 		expect(response.status).toBe(201)
 	}, 10000)
 
+
+	test("Registro de usuário com campos em branco", async() =>{
+		const newUser = {
+			name: "Maria da Silva",
+			email: "mariasilva@gmail.com",
+			password: "Senha2023"
+		}
+		const response = await request(app).post("/users").set("Content-type", "application/json").send({name: newUser.name, email: newUser.email, password: newUser.password})
+
+		const receivedUser = response.body 
+
+		expect({
+			email: newUser.email,
+			name: newUser.name,
+			password: newUser.password
+		}).toEqual({
+			email: receivedUser.email,
+			name: receivedUser.name,
+			password: receivedUser.password
+		})
+		expect(response.status).toBe(400)
+	}, 10000)
+
+	test("Usuário pode fazer upgrade para proprietário com chave pix válida", async () => {
+		const pixKey = "111.222.333-44"
+	
+		const response = await request(app)
+			.post("/upgrade")
+			.set("Authorization", `Bearer ${token}`)
+			.send({ pixKey })
+	
+		expect(response.status).toBe(200)
+	
+		expect(response.body.message).toBe("Upgrade de conta realizado com sucesso")
+	
+		await prisma.owner.findUnique({
+			where: {
+				userId: response.body.userId
+			},
+		})
+	})
 })
--- src/service/reservationService.ts
+++ src/service/reservationService.ts
@@ -1,158 +1,158 @@
-import { prisma } from '../database/prisma';
-import { ReservationPaymentStatus } from '../model/reservationPaymentStatus';
-import { CheckReservationAvailability } from '../utils/checkReservationAvailability';
+import { prisma } from "../database/prisma"
+import { ReservationPaymentStatus } from "../model/reservationPaymentStatus"
+import { CheckReservationAvailability } from "../utils/checkReservationAvailability"
 
 export class ReservationService {
-    async create(userId: string, parkingSpaceId: string, startDate: string, endDate: string, startTime: string, endTime: string) {
+	async create(userId: string, parkingSpaceId: string, startDate: string, endDate: string, startTime: string, endTime: string) {
 
-        const isAvaiable = await CheckReservationAvailability(parkingSpaceId, startDate, startTime, endDate, endTime);
+		const isAvaiable = await CheckReservationAvailability(parkingSpaceId, startDate, startTime, endDate, endTime)
 
-        if (!isAvaiable) {
-            throw new Error("parking space already ocupied");
-        }
+		if (!isAvaiable) {
+			throw new Error("parking space already ocupied")
+		}
 
-        const newReservation = await prisma.reservation.create({
-            data: {
-                userId,
-                parkingSpaceId,
-                startTime,
-                endTime,
-                startDate,
-                endDate
-            }
-        });
+		const newReservation = await prisma.reservation.create({
+			data: {
+				userId,
+				parkingSpaceId,
+				startTime,
+				endTime,
+				startDate,
+				endDate
+			}
+		})
 
-        return newReservation;
+		return newReservation
 
-    }
-    async listOwner(id: string) {
-        const owner = await prisma.owner.findUnique({
-            where: {
-                userId: id
-            }
-        });
+	}
+	async listOwner(id: string) {
+		const owner = await prisma.owner.findUnique({
+			where: {
+				userId: id
+			}
+		})
 
-        if (!owner) {
-            throw new Error("user not owner");
-        }
+		if (!owner) {
+			throw new Error("user not owner")
+		}
 
-        const reservations = await prisma.reservation.findMany({
-            where: {
-                parkingSpace: {
-                    ownerId: id
-                }
-            }
-        });
+		const reservations = await prisma.reservation.findMany({
+			where: {
+				parkingSpace: {
+					ownerId: id
+				}
+			}
+		})
 
-        return reservations;
+		return reservations
 
-    }
-    async listAll(id: string) {
-        const reservations = await prisma.reservation.findMany({
-            where: {
-                userId: id
-            }
-        });
+	}
+	async listAll(id: string) {
+		const reservations = await prisma.reservation.findMany({
+			where: {
+				userId: id
+			}
+		})
 
-        return reservations;
-    }
-    async delete(id: string) {
-        const reservation = await prisma.reservation.findUnique({
-            where: {
-                id: id
-            }
-        });
+		return reservations
+	}
+	async delete(id: string) {
+		const reservation = await prisma.reservation.findUnique({
+			where: {
+				id: id
+			}
+		})
 
-        if (!reservation) {
-            throw new Error("reservation does not exist");
-        }
+		if (!reservation) {
+			throw new Error("reservation does not exist")
+		}
 
-        const deletedReservation = await prisma.reservation.delete({
-            where: {
-                id: id
-            }
-        })
+		const deletedReservation = await prisma.reservation.delete({
+			where: {
+				id: id
+			}
+		})
 
-        return deletedReservation;
-    }
-    async updateStatusPayment(id: string, newStatus: string) {
+		return deletedReservation
+	}
+	async updateStatusPayment(id: string, newStatus: string) {
 
-        const reservation = await prisma.reservation.findUnique({
-            where: {
-                id
-            }
-        })
+		const reservation = await prisma.reservation.findUnique({
+			where: {
+				id
+			}
+		})
 
-        if (!reservation) {
-            throw new Error("reservation does not exist");
-        }
+		if (!reservation) {
+			throw new Error("reservation does not exist")
+		}
 
-        if (!Object.values(ReservationPaymentStatus).includes(newStatus as ReservationPaymentStatus)) {
-            throw new Error("reservation status invalid");
-        }
+		if (!Object.values(ReservationPaymentStatus).includes(newStatus as ReservationPaymentStatus)) {
+			throw new Error("reservation status invalid")
+		}
 
-        const updatedStatusPayment = await prisma.reservation.update({
-            where: {
-                id
-            },
-            data: {
-                paymentStatus: newStatus as ReservationPaymentStatus
-            }
-        });
+		const updatedStatusPayment = await prisma.reservation.update({
+			where: {
+				id
+			},
+			data: {
+				paymentStatus: newStatus as ReservationPaymentStatus
+			}
+		})
 
-        return updatedStatusPayment;
+		return updatedStatusPayment
 
-    }
-    async updateReservationDate(id: string, endTime: string, endDate: string) {
+	}
+	async updateReservationDate(id: string, endTime: string, endDate: string) {
 
-        const reservation = await prisma.reservation.findUnique({
-            where: {
-                id
-            }
-        });
+		const reservation = await prisma.reservation.findUnique({
+			where: {
+				id
+			}
+		})
 
-        if (!reservation) {
-            throw new Error("reservation does not exist");
-        }
+		if (!reservation) {
+			throw new Error("reservation does not exist")
+		}
 
-        const newEndDate = new Date(`${endDate}T${endTime}`);
-        const oldEndDate = new Date(`${reservation.endDate}T${reservation.endTime}`);
+		const newEndDate = new Date(`${endDate}T${endTime}`)
+		const oldEndDate = new Date(`${reservation.endDate}T${reservation.endTime}`)
 
-        if (oldEndDate.getDate() - newEndDate.getDate() < 0) {
+		if (oldEndDate.getDate() - newEndDate.getDate() < 0) {
 
-            if (reservation.paymentStatus === ReservationPaymentStatus.Pendente) {
+			if (reservation.paymentStatus === ReservationPaymentStatus.Pendente) {
 
-                const updatedReservation = await prisma.reservation.update({
-                    where: {
-                        id
-                    },
-                    data: {
-                        endTime,
-                        endDate,
-                        paymentStatus: ReservationPaymentStatus.Pendente
-                    }
-                });
+				const updatedReservation = await prisma.reservation.update({
+					where: {
+						id
+					},
+					data: {
+						endTime,
+						endDate,
+						paymentStatus: ReservationPaymentStatus.Pendente
+					}
+				})
 
-                return updatedReservation;
-            }
+				return updatedReservation
+			}
 
-            else {
-                throw new Error("invalid end time");
-            }
-        }
+			else {
+				throw new Error("invalid end time")
+			}
+		}
 
-        const updatedReservation = prisma.reservation.update({
-            where: {
-                id
-            },
-            data: {
-                endTime,
-                endDate,
-                paymentStatus: ReservationPaymentStatus.Pendente
-            }
-        });
+		const updatedReservation = prisma.reservation.update({
+			where: {
+				id
+			},
+			data: {
+				endTime,
+				endDate,
+				paymentStatus: ReservationPaymentStatus.Pendente
+			}
+		})
 
-        return updatedReservation;
+		return updatedReservation
 
-    }
+	}
 }
